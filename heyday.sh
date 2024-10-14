485e78578403f47596b9aecfbafffcc16aaf894f
Elanah J Schmitz <ezs0148@auburn.edu>
2024-10-14 11:51:32 -0500

    Update heyday.sh
    changed greeting

--- heyday.sh
+++ heyday.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# hey_day.sh
+# heyday.sh
 # A Bash script to celebrate Auburn's Annual "Hey Day"!
 
 # Ask the user for their name
 read -p "What's your name? " name
 
 # Greet the user with "Hey" and their name, adding a fun message
-echo "Hey $name! Welcome to Auburn's Annual Hey Day! Hope you're having a fantastic day full of smiles!"
+echo "Hey $name! Welcome to Auburn's Annual Hey Day! Hope you have a fantastic day!"
